9537b11330cb5b757c0c131814685c5d38af876b
Joni Pohjaniemi <joni.pohjaniemi@gmail.com>
2023-09-10 16:24:06 +0200

    Initial project

---
+++ src/test/java/joni/thales/transactions_api/TransactionsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package joni.thales.transactions_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
